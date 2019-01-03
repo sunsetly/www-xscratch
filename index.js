c169518484a8a66e71d8eeedb8a9bc666a76341f
encloinc <jelch2002@gmail.com>
2019-01-02 21:27:09 -0600

    Now it works, lets do this

--- index.js
+++ index.js
@@ -1,13 +1,32 @@
 // The beginning of something great.
 
-var subdomain = require('express-subdomain');
-var express = require('express');
-var app = express();
+const express = require('express');
+const path = require('path')
+const app = express();
  
 
 app.use(express.static(path.join(__dirname, "client")))
 
+app.use(function (req, res, next) {
 
-app.get('/', function(req, res) {
-    res.redirect(path.join(__dirname, '/client/home/index.html'))
+    res.setHeader('Access-Control-Allow-Origin', '*');
+
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+    res.setHeader('Access-Control-Allow-Headers', '*');
+
+    res.setHeader('Access-Control-Allow-Credentials', true);
+
+    next();
+});
+
+
+app.listen(80, () => console.log('Running xScratch'))
+
+app.get('/', (req, res) => {  
+	res.redirect("/home")
+});
+
+app.get('/home', (req, res) => {
+	res.redirect(path.join(__dirname, "client/home/index.html"))
 });
